1dac9f3db9c77fb5ae6b58dfbee1953a93eb00cc
Claudio Acioli <kllaudyo@gmail.com>
2018-01-09 07:58:18 -0200

    feat: Usa componente Home

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Home from './Home';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
         const { posts } = this.props;
         return (
             <div className="App">
+                <Home />
                 { posts.filter( post => !post.delete ).map( post => {
                     return (
                         <div key={post.id}>
